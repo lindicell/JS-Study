64177e0eb9721df67089033e9ba75a1aa8665cdc
Lindice <Lindicell@hotmail.com>
2021-02-27 16:38:05 -0300

    Projeto final

--- JS-Study/BitBank/Cliente.js
+++ JS-Study/BitBank/Cliente.js
@@ -1,16 +1,18 @@
 export class Cliente {
-    nome;
-    _cpf;
 
+	get cpf(){
+  		return  this._cpf;
+	}
 
+	constructor(nome,cpf, senha){
+    	this.nome = nome;
+    	this._cpf = cpf;
+    	this._senha = senha;
+	}	
 
-get cpf(){
-  return  this._cpf;
-}
+	autenticar(){
+		return true;
+	}
 
-constructor(nome,cpf){
-    this.nome = nome;
-    this._cpf = cpf;
-}
 
 }
--- JS-Study/BitBank/index.js
+++ JS-Study/BitBank/index.js
@@ -1,16 +1,19 @@
 import { Cliente } from "./Cliente.js"
-import { ContaCorrente } from "./ContaCorrente.js"
+import {Gerente} from "./Funcionarios/Gerente.js"
+import {Diretor} from "./Funcionarios/Diretor.js"
+import {SistemaAutenticacao} from "./SistemaAutenticacao.js"
 
 
-const clientex = new Cliente("Teste", 1234567890);
-const clientey = new Cliente("ABC", 1234555890);
+const diretor = new Diretor("Rodrigo",10000, 12345678900);
+diretor.cadastrarSenha("123");
+const gerente = new Gerente("Luis",50000, 12345678901);
+gerente.cadastrarSenha("1234");
 
-const ContaCorrenteTeste = new ContaCorrente(clientex, 10);
-ContaCorrenteTeste.depositar(100);
+const cliente = new Cliente("Lais", 123456,"12345");
 
-const ContaCorrenteTestey = new ContaCorrente(clientey, 20);
-ContaCorrenteTeste.transferir(50, ContaCorrenteTestey);
+const estaLogado = SistemaAutenticacao.login(diretor,"123");
+const estaLogado1 = SistemaAutenticacao.login(gerente,"1234");
+const estaLogado2 = SistemaAutenticacao.login(cliente,"12345");
 
 
-
-console.log(ContaCorrente.numeroDeContas);
+console.log(estaLogado, estaLogado1, estaLogado2);
